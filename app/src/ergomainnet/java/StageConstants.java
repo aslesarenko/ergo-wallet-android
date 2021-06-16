c12716ac471e957e3eacf5ad1db56144837e6416
Benjamin Schulte <bs@golfgl.de>
2021-06-16 18:02:29 +0200

    appkit explorer api update

--- app/src/ergomainnet/java/StageConstants.java
+++ app/src/ergomainnet/java/StageConstants.java
@@ -5,7 +5,7 @@ public class StageConstants {
     public static final NetworkType NETWORK_TYPE = NetworkType.MAINNET;
 
     public static final String EXPLORER_WEB_ADDRESS = "https://explorer.ergoplatform.com/";
-    public static final String EXPLORER_API_ADDRESS = "https://api.ergoplatform.com/api/";
+    public static final String EXPLORER_API_ADDRESS = "https://api.ergoplatform.com/";
 
     public static final String NODE_API_ADDRESS = "http://213.239.193.208:9053/";
 
--- app/src/ergotestnet/java/StageConstants.java
+++ app/src/ergotestnet/java/StageConstants.java
@@ -5,7 +5,7 @@ public class StageConstants {
     public static final NetworkType NETWORK_TYPE = NetworkType.TESTNET;
 
     public static final String EXPLORER_WEB_ADDRESS = "https://testnet.ergoplatform.com/";
-    public static final String EXPLORER_API_ADDRESS = "https://api-testnet.ergoplatform.com/api/";
+    public static final String EXPLORER_API_ADDRESS = "https://api-testnet.ergoplatform.com/";
 
     public static final String NODE_API_ADDRESS = "http://213.239.193.208:9052/";
 
